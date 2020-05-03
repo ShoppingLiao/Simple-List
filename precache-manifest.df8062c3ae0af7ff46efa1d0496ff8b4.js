831cf65ec9cb14636f2fe1a4fe4785c82a0b591d
ShoppingLiao <tom0801a@gmail.com>
2020-05-03 14:25:53 +0800

    Updates

---
+++ precache-manifest.df8062c3ae0af7ff46efa1d0496ff8b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "571cec609cf99adaece72966a9b4a26c",
+    "url": "/Simple-List/index.html"
+  },
+  {
+    "revision": "4404b69323f14e15cac4",
+    "url": "/Simple-List/static/css/main.08e1dbec.chunk.css"
+  },
+  {
+    "revision": "e1f4ffc497ffc664256d",
+    "url": "/Simple-List/static/js/2.55546847.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Simple-List/static/js/2.55546847.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4404b69323f14e15cac4",
+    "url": "/Simple-List/static/js/main.912a7a3f.chunk.js"
+  },
+  {
+    "revision": "39135f514874fd17cf93",
+    "url": "/Simple-List/static/js/runtime-main.9852fd9d.js"
+  }
+]);
